4c48aeb47aa9c88e3ce7f879021a1650404a7f9c
LarsSchmidbauer <l.schmidbauer@xdev-software.de>
2024-11-15 12:07:20 +0100

    Created CESOP Message for Demo

--- bzst-dip-java-client-demo/src/main/java/software/xdev/ApplicationWithCesop.java
+++ bzst-dip-java-client-demo/src/main/java/software/xdev/ApplicationWithCesop.java
@@ -1,7 +1,14 @@
 package software.xdev;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.UUID;
+
+import javax.xml.datatype.DatatypeConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +19,37 @@ import software.xdev.bzst.dip.client.model.configuration.BzstDipConfiguration;
 import software.xdev.bzst.dip.client.model.configuration.BzstDipConfigurationBuilder;
 import software.xdev.bzst.dip.client.model.configuration.BzstDipDpiMessageType;
 import software.xdev.bzst.dip.client.model.configuration.BzstDipOecdDocType;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopAccountIdentifier;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopAccountIdentifierTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopAdress;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopAmount;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopDocSpec;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopDocTypeIndicEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopLegalAdressTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopMSCountryCodeTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopMessageTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopMessageTypeIndicEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopName;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopNameTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPSPIdType;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPSPIdTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPSPRole;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPSPRoleTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPayerMS;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPaymentDataBody;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPaymentMethod;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopPaymentMethodEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopReportedPayee;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopReportedTransaction;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopReportingPSP;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopTAXIdentification;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopTransactionDateType;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopTransactionDateTypeEnum;
+import software.xdev.bzst.dip.client.model.message.cesop.BzstCesopVatId;
 import software.xdev.bzst.dip.client.model.message.dac7.BzstDipAddressFix;
 import software.xdev.bzst.dip.client.model.message.dac7.BzstDipCompleteResult;
-import software.xdev.bzst.dip.client.model.message.dac7.BzstDipMessage;
+import software.xdev.bzst.dip.client.model.message.dac7.BzstDipCountryCode;
+import software.xdev.bzst.dip.client.model.message.dac7.BzstDipCurrency;
 
 
 @SuppressWarnings("checkstyle:MagicNumber")
@@ -23,7 +58,7 @@ public final class ApplicationWithCesop
 	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationWithCesop.class);
 	
 	public static void main(final String[] args)
-		throws InterruptedException, HttpStatusCodeNotExceptedException, IOException
+		throws InterruptedException, HttpStatusCodeNotExceptedException, IOException, DatatypeConfigurationException
 	{
 		final BzstDipConfiguration configuration = createConfiguration();
 		final BzstDipClient bzstDipClient = new BzstDipClient(configuration);
@@ -52,13 +87,52 @@ public final class ApplicationWithCesop
 			.setPlatformOperatorOrganizationName("TestOrg")
 			.setPlatformOperatorPlatformName("TestApp")
 			.setPlatformOperatorAddress(new BzstDipAddressFix("TestCity"))
+			.setTransmittingCountry(BzstDipCountryCode.DE)
+			.setMessageType(BzstCesopMessageTypeEnum.PMT)
+			.setReportingPeriodCesopQuarter(2)
+			.setReportingPeriodCesopYear("2024")
+			.setMessageRefId(UUID.randomUUID().toString())
+			.setMessageTypeIndicCesop(BzstCesopMessageTypeIndicEnum.CESOP_100)
+			.setTimestamp(ZonedDateTime.parse("2024-05-08T14:02:53Z", DateTimeFormatter.ISO_ZONED_DATE_TIME))
 			.buildAndValidate();
 	}
 	
-	public static BzstDipMessage createMessage()
+	public static BzstCesopPaymentDataBody createMessage()
 	{
-		return new BzstDipMessage(
-			// TODO Build CESOP Message
+		return new BzstCesopPaymentDataBody(
+			new BzstCesopReportingPSP(
+				new BzstCesopPSPIdType(BzstCesopPSPIdTypeEnum.BIC, "TRSYDEFFXXX"),
+				new BzstCesopName(BzstCesopNameTypeEnum.LEGAL, "360 TREASURY SYSTEMS AG")
+			),
+			List.of(
+				new BzstCesopReportedPayee(
+					new BzstCesopName(BzstCesopNameTypeEnum.BUSINESS, "German Company AG"),
+					BzstDipCountryCode.DE,
+					new BzstCesopAdress(
+						BzstCesopLegalAdressTypeEnum.CESOP_303,
+						BzstDipCountryCode.DE,
+						"Beispielstraße 3, 11712 Berlin"),
+					new BzstCesopTAXIdentification(new BzstCesopVatId(
+						BzstCesopMSCountryCodeTypeEnum.DE,
+						"112233445")),
+					new BzstCesopAccountIdentifier(
+						BzstDipCountryCode.DE,
+						BzstCesopAccountIdentifierTypeEnum.IBAN,
+						"DE32503302010298200040"),
+					new BzstCesopReportedTransaction(false, "DE-01-0101",
+						new BzstCesopTransactionDateType(
+							BzstCesopTransactionDateTypeEnum.CESOP_701,
+							ZonedDateTime.parse("2024-03-03T11:53:25Z", DateTimeFormatter.ISO_ZONED_DATE_TIME)),
+						new BzstCesopAmount(BzstDipCurrency.EUR, new BigDecimal("100")),
+						new BzstCesopPaymentMethod(BzstCesopPaymentMethodEnum.BANK_TRANSFER),
+						false,
+						new BzstCesopPayerMS(
+							BzstCesopAccountIdentifierTypeEnum.IBAN,
+							BzstCesopMSCountryCodeTypeEnum.FR),
+						new BzstCesopPSPRole(BzstCesopPSPRoleTypeEnum.MONEY_TRANSFER_OPERATOR)),
+					new BzstCesopDocSpec(BzstCesopDocTypeIndicEnum.CESOP_1, UUID.randomUUID())
+				)
+			)
 		);
 	}
 	
