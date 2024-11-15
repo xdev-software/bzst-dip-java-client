22584b83426f1a4e55c32ef5754af36d59973131
LarsSchmidbauer <l.schmidbauer@xdev-software.de>
2024-11-15 12:10:56 +0100

    removed unused value

--- bzst-dip-java-client/src/main/java/software/xdev/bzst/dip/client/model/message/cesop/BzstCesopTAXIdentification.java
+++ bzst-dip-java-client/src/main/java/software/xdev/bzst/dip/client/model/message/cesop/BzstCesopTAXIdentification.java
@@ -4,6 +4,7 @@ package software.xdev.bzst.dip.client.model.message.cesop;
  * @param vatId The VAT identification number of the payee confirmed by the EU identification number of the paye
  * @param value The Tax ID.
  */
-public record BzstCesopTAXIdentification(BzstCesopVatId vatId, String value)
+public record BzstCesopTAXIdentification(BzstCesopVatId vatId)
 {
 }
+
