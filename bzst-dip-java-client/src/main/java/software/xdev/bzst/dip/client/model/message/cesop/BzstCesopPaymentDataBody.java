dc9c4cb90618218adc402b427c3a3f3d713efeab
LarsSchmidbauer <l.schmidbauer@xdev-software.de>
2024-11-15 12:10:36 +0100

    Extract methods for better reading and switched the reported payees to a list, because you can send more than one report

--- bzst-dip-java-client/src/main/java/software/xdev/bzst/dip/client/model/message/cesop/BzstCesopPaymentDataBody.java
+++ bzst-dip-java-client/src/main/java/software/xdev/bzst/dip/client/model/message/cesop/BzstCesopPaymentDataBody.java
@@ -1,6 +1,8 @@
 package software.xdev.bzst.dip.client.model.message.cesop;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
@@ -32,20 +34,123 @@ import software.xdev.bzst.dip.client.xmldocument.model.cesop.TransactionDateType
 import software.xdev.bzst.dip.client.xmldocument.model.cesop.TransactionDateTypeType;
 import software.xdev.bzst.dip.client.xmldocument.model.cesop.VATIdType;
 
-// TODO: Das in Methoden auslagern, für bessere Lesbarkeit
-
 
 /**
  * @param reportingPSP  The reportingPSP element clearly defines the payment service provider that reports the payment
  *                      data to the national tax administration. {@link BzstCesopReportingPSP}
  * @param reportedPayee defines the payee to whom the data transmitted by the payment service provider
  */
-public record BzstCesopPaymentDataBody(BzstCesopReportingPSP reportingPSP, BzstCesopReportedPayee reportedPayee)
+public record BzstCesopPaymentDataBody(BzstCesopReportingPSP reportingPSP, List<BzstCesopReportedPayee> reportedPayees)
 {
 	public PaymentDataBodyType toXmlType() throws DatatypeConfigurationException
 	{
 		final PaymentDataBodyType paymentDataBodyType = new PaymentDataBodyType();
 		
+		paymentDataBodyType.setReportingPSP(this.convertToPSPType());
+		
+		this.convertToReportedPayeesType();
+		for(final ReportedPayeeType reportedPayeeType : this.convertToReportedPayeesType())
+		{
+			paymentDataBodyType.getReportedPayee().add(reportedPayeeType);
+		}
+		
+		return paymentDataBodyType;
+	}
+	
+	private List<ReportedPayeeType> convertToReportedPayeesType() throws DatatypeConfigurationException
+	{
+		final List<ReportedPayeeType> reportedPayeeTypes = new ArrayList<>();
+		for(final BzstCesopReportedPayee reportedPayee : this.reportedPayees)
+		{
+			final ReportedPayeeType reportedPayeeType = new ReportedPayeeType();
+			
+			final NameType nameTypeReportedPayeeType = new NameType();
+			final DocSpecType docSpecType = new DocSpecType();
+			final AccountIdentifierType accountIdentifierType = new AccountIdentifierType();
+			final TAXIdentifierType taxIdentifierType = new TAXIdentifierType();
+			final VATIdType vatIdType = new VATIdType();
+			final ReportedTransactionType reportedTransactionType = new ReportedTransactionType();
+			final AmountType amountType = new AmountType();
+			final PaymentMethodType paymentMethodType = new PaymentMethodType();
+			final PayerMSType payerMSType = new PayerMSType();
+			final PSPRoleType pspRoleType = new PSPRoleType();
+			final TransactionDateType transactionDateType = new TransactionDateType();
+			
+			nameTypeReportedPayeeType.setValue(reportedPayee.name().value());
+			nameTypeReportedPayeeType.setNameType(NameTypeEnumType.fromValue(reportedPayee.name()
+				.nameType()
+				.value()));
+			docSpecType.setDocTypeIndic(DocTypeIndicEnumType.fromValue(reportedPayee.docSpec()
+				.docTypeIndicEnum()
+				.value()));
+			docSpecType.setDocRefId(reportedPayee.docSpec().docRefId().toString());
+			accountIdentifierType.setType(AccountIdentifierTypeType.fromValue(reportedPayee.accountIdentifier()
+				.accountIdentifierType()
+				.value()));
+			accountIdentifierType.setValue(reportedPayee.accountIdentifier().value());
+			accountIdentifierType.setCountryCode(CountryCodeType.fromValue(reportedPayee.countryCode().name()));
+			vatIdType.setValue(reportedPayee.taxIdentification().vatId().value());
+			vatIdType.setIssuedBy(MSCountryCodeType.fromValue(reportedPayee.taxIdentification()
+				.vatId()
+				.msCountryCodeTypeEnum()
+				.value()));
+			taxIdentifierType.getVATId().add(vatIdType);
+			amountType.setValue(reportedPayee.reportedTransaction().amount().amountValue());
+			amountType.setCurrency(CurrCodeType.fromValue(reportedPayee.reportedTransaction()
+				.amount()
+				.currency()
+				.name()));
+			reportedTransactionType.setAmount(amountType);
+			reportedTransactionType.setTransactionIdentifier(reportedPayee.reportedTransaction()
+				.transactionIdentifier());
+			paymentMethodType.setPaymentMethodType(PaymentMethodTypeType.valueOf(reportedPayee.reportedTransaction()
+				.paymentMethod()
+				.paymentMethodEnum()
+				.value()));
+			payerMSType.setPayerMSSource(AccountIdentifierTypeType.valueOf(reportedPayee.reportedTransaction()
+				.payerMS()
+				.accountIdentifierType()
+				.value()));
+			payerMSType.setValue(MSCountryCodeType.valueOf(reportedPayee.reportedTransaction()
+				.payerMS()
+				.msCountryCodeType()
+				.value()));
+			pspRoleType.setPSPRoleType(PSPRoleTypeType.valueOf(reportedPayee.reportedTransaction()
+				.pspRole()
+				.pspRoleType()
+				.value()));
+			reportedTransactionType.setPaymentMethod(paymentMethodType);
+			reportedTransactionType.setIsRefund(reportedPayee.reportedTransaction().isRefund());
+			reportedTransactionType.setPayerMS(payerMSType);
+			reportedTransactionType.setPSPRole(pspRoleType);
+			reportedTransactionType.setInitiatedAtPhysicalPremisesOfMerchant(reportedPayee.reportedTransaction()
+				.initiatedAtPhysicalPremisesOfMerchant());
+			
+			final GregorianCalendar gregorianCalendar =
+				GregorianCalendar.from(reportedPayee.reportedTransaction().transactionDateType().value());
+			final XMLGregorianCalendar xmlGregorianCalendar =
+				DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+			
+			transactionDateType.setValue(xmlGregorianCalendar);
+			transactionDateType.setTransactionDateType(TransactionDateTypeType.valueOf(reportedPayee.reportedTransaction()
+				.transactionDateType()
+				.transactionDateTypeEnum()
+				.value()));
+			reportedTransactionType.getDateTime().add(transactionDateType);
+			
+			reportedPayeeType.getName().add(nameTypeReportedPayeeType);
+			reportedPayeeType.setCountry(CountryCodeType.fromValue(reportedPayee.countryCode().name()));
+			reportedPayeeType.setDocSpec(docSpecType);
+			reportedPayeeType.setAccountIdentifier(accountIdentifierType);
+			reportedPayeeType.setTAXIdentification(taxIdentifierType);
+			reportedPayeeType.getReportedTransaction().add(reportedTransactionType);
+			reportedPayeeTypes.add(reportedPayeeType);
+		}
+		return reportedPayeeTypes;
+	}
+	
+	private PSPType convertToPSPType()
+	{
 		final PSPType pspType = new PSPType();
 		final PSPIdType pspIdType = new PSPIdType();
 		final NameType nameType = new NameType();
@@ -56,92 +161,6 @@ public record BzstCesopPaymentDataBody(BzstCesopReportingPSP reportingPSP, BzstC
 		nameType.setValue(this.reportingPSP.name().value());
 		pspType.setPSPId(pspIdType);
 		pspType.getName().add(nameType);
-		
-		paymentDataBodyType.setReportingPSP(pspType);
-		
-		final ReportedPayeeType reportedPayeeType = new ReportedPayeeType();
-		
-		final NameType nameTypeReportedPayeeType = new NameType();
-		final DocSpecType docSpecType = new DocSpecType();
-		final AccountIdentifierType accountIdentifierType = new AccountIdentifierType();
-		final TAXIdentifierType taxIdentifierType = new TAXIdentifierType();
-		final VATIdType vatIdType = new VATIdType();
-		final ReportedTransactionType reportedTransactionType = new ReportedTransactionType();
-		final AmountType amountType = new AmountType();
-		final PaymentMethodType paymentMethodType = new PaymentMethodType();
-		final PayerMSType payerMSType = new PayerMSType();
-		final PSPRoleType pspRoleType = new PSPRoleType();
-		final TransactionDateType transactionDateType = new TransactionDateType();
-		
-		nameTypeReportedPayeeType.setValue(this.reportedPayee.name().value());
-		nameTypeReportedPayeeType.setNameType(NameTypeEnumType.fromValue(this.reportedPayee.name().nameType().value()));
-		docSpecType.setDocTypeIndic(DocTypeIndicEnumType.fromValue(this.reportedPayee.docSpec()
-			.docTypeIndicEnum()
-			.value()));
-		docSpecType.setDocRefId(this.reportedPayee.docSpec().docRefId().toString());
-		accountIdentifierType.setType(AccountIdentifierTypeType.fromValue(this.reportedPayee.accountIdentifier()
-			.accountIdentifierType()
-			.value()));
-		accountIdentifierType.setValue(this.reportedPayee.accountIdentifier().value());
-		accountIdentifierType.setCountryCode(CountryCodeType.fromValue(this.reportedPayee.countryCode().name()));
-		vatIdType.setValue(this.reportedPayee.taxIdentification().vatId().value());
-		vatIdType.setIssuedBy(MSCountryCodeType.fromValue(this.reportedPayee.taxIdentification()
-			.vatId()
-			.msCountryCodeTypeEnum()
-			.value()));
-		taxIdentifierType.getVATId().add(vatIdType);
-		amountType.setValue(this.reportedPayee.reportedTransaction().amount().amountValue());
-		amountType.setCurrency(CurrCodeType.fromValue(this.reportedPayee.reportedTransaction()
-			.amount()
-			.currency()
-			.name()));
-		reportedTransactionType.setAmount(amountType);
-		reportedTransactionType.setTransactionIdentifier(this.reportedPayee.reportedTransaction()
-			.transactionIdentifier());
-		paymentMethodType.setPaymentMethodType(PaymentMethodTypeType.valueOf(this.reportedPayee.reportedTransaction()
-			.paymentMethod()
-			.paymentMethodEnum()
-			.value()));
-		payerMSType.setPayerMSSource(AccountIdentifierTypeType.valueOf(this.reportedPayee.reportedTransaction()
-			.payerMS()
-			.accountIdentifierType()
-			.value()));
-		payerMSType.setValue(MSCountryCodeType.valueOf(this.reportedPayee.reportedTransaction()
-			.payerMS()
-			.msCountryCodeType()
-			.value()));
-		pspRoleType.setPSPRoleType(PSPRoleTypeType.valueOf(this.reportedPayee.reportedTransaction()
-			.pspRole()
-			.pspRoleType()
-			.value()));
-		reportedTransactionType.setPaymentMethod(paymentMethodType);
-		reportedTransactionType.setIsRefund(this.reportedPayee.reportedTransaction().isRefund());
-		reportedTransactionType.setPayerMS(payerMSType);
-		reportedTransactionType.setPSPRole(pspRoleType);
-		reportedTransactionType.setInitiatedAtPhysicalPremisesOfMerchant(this.reportedPayee.reportedTransaction()
-			.initiatedAtPhysicalPremisesOfMerchant());
-		
-		final GregorianCalendar gregorianCalendar =
-			GregorianCalendar.from(this.reportedPayee.reportedTransaction().transactionDateType().value());
-		final XMLGregorianCalendar xmlGregorianCalendar =
-			DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
-		
-		transactionDateType.setValue(xmlGregorianCalendar);
-		transactionDateType.setTransactionDateType(TransactionDateTypeType.valueOf(this.reportedPayee.reportedTransaction()
-			.transactionDateType()
-			.transactionDateTypeEnum()
-			.value()));
-		reportedTransactionType.getDateTime().add(transactionDateType);
-		
-		reportedPayeeType.getName().add(nameTypeReportedPayeeType);
-		reportedPayeeType.setCountry(CountryCodeType.fromValue(this.reportedPayee.countryCode().name()));
-		reportedPayeeType.setDocSpec(docSpecType);
-		reportedPayeeType.setAccountIdentifier(accountIdentifierType);
-		reportedPayeeType.setTAXIdentification(taxIdentifierType);
-		reportedPayeeType.getReportedTransaction().add(reportedTransactionType);
-		
-		paymentDataBodyType.getReportedPayee().add(reportedPayeeType);
-		
-		return paymentDataBodyType;
+		return pspType;
 	}
 }
